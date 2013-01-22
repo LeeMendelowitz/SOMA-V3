0ab6e6d3997c4c5665d052bdf1f595d22da61670
Lee Mendelowitz <lmendelo@umiacs.umd.edu>
2013-01-22 16:29:46 -0500

    Added FragDatabase class to significanceTest.py

---
+++ bin/significanceTest.py
@@ -0,0 +1,50 @@
+##################################
+# Author: Lee Mendelowitz (lmendelo@umiacs.umd.edu)
+# Date: 1/22/2013
+# File: significanceTest.py
+#
+# Description:
+
+# Some code to generate random contig maps by sampling 
+# with replacement from a distribution of contig fragments.
+import importSomaData
+import numpy as np
+import sys
+
+##############################################################################
+# Class to store fragments
+class FragDataBase(object):
+
+    def __init__(self, *args, **kwargs):
+        self.interiorFrags = np.array([])
+        self.allFrags = np.array([])
+        if 'mapFile' in kwargs:
+            sys.stdout.write('Adding frags from map %s...'%kwargs['mapFile'])
+            self.addFragsFromMap(kwargs['mapFile'])
+            sys.stdout.write('Added %i fragments\n'%len(self.allFrags))
+
+    def addInteriorFrags(self, frags):
+        self.interiorFrags = np.array(list(self.interiorFrags) + list(frags))
+
+    def addFrags(self, frags):
+        self.allFrags = np.array(list(self.allFrags) + list(frags))
+
+    # Sample n restriction fragments (with replacement)
+    def sampleFrags(self, n):
+        ind = np.random.randint(0, self.allFrags.shape[0], n)
+        return self.allFrags[ind]
+        
+    # Sample n interior restriction fragments (with replacement)
+    def sampleInteriorFrags(self, n):
+        ind = np.random.randint(0, self.interiorFrags.shape[0], n)
+        return self.interiorFrags[ind]
+
+    def addFragsFromMap(self, mapFileName):
+        mapDict = importSomaData.readMapFile(mapFileName)
+        for mapId, mapObj in mapDict.iteritems():
+            frags = mapObj.frags
+            if not frags:
+                continue
+            self.addFrags(frags)
+            if len(frags) > 2:
+                self.addInteriorFrags(frags[1:-1])
--- test/testMatch/runMatch.sh
+++ test/testMatch/runMatch.sh
@@ -4,7 +4,8 @@ MATCH_BIN_DEBUG=$BIN_DIR/match_d
 MATCH_BIN_RELEASE=$BIN_DIR/match
 
 # TEST RELEASE
-$MATCH_BIN_RELEASE --output test100WithError --pvalue=0.05 --sdMin=4 --sdMax=4 --numPermutationTrials 1000 --numThreads 4 contigs.silico map100.opt
+#$MATCH_BIN_RELEASE --output test100WithError --pvalue=0.05 --sdMin=4 --sdMax=4 --numPermutationTrials 1000 --numThreads 4 contigs.silico map100.opt
+$MATCH_BIN_RELEASE --output test100_maxMatch1 --maxMatchesPerContig=1  --pvalue=0.05 --sdMin=4 --sdMax=4 --numPermutationTrials 0 --numThreads 4 contigs.silico map100.opt
 
 # TEST DEBUG
 #$MATCH_BIN_DEBUG --output test100WithError --pvalue=0.05 --sdMin=4 --sdMax=4 --numPermutationTrials 0 --numThreads 1 contigs.silico map100.opt
