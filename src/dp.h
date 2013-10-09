4c16a1ded5289ccc25791acc004a8cef30641db9
Lee Mendelowitz <LMendelo@umiacs.umd.edu>
2013-10-08 21:55:55 -0400

    Incremental commit

--- src/dp.h
+++ src/dp.h
@@ -14,6 +14,8 @@ class AlignmentParams;
 // if match_others is true, then put other match results in MatchResult
 MatchResult * match(const ContigMapData * pContigMap, const OpticalMapData * pOpticalMap,
                      std::vector<MatchResult *> * pOthers, const AlignmentParams& alignParams);
+MatchResult * match2(const ContigMapData * pContigMap, const OpticalMapData * pOpticalMap,
+                     std::vector<MatchResult *> * pOthers, const AlignmentParams& alignParams);
 
 // Light-weight alignment for permutation test.
 // This alignment avoids filtering and avoids the construction of MatchResults.
